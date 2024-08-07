50a333d5c0ee54991baeaba9b6515468f1ed3cad
vinh345 <vinh77r@gmail.com>
2024-08-07 08:14:36 +0700

    Commit các thay đổi ở nhánh login

--- src/main/java/com/example/ojt/controller/AuthController.java
+++ src/main/java/com/example/ojt/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.example.ojt.controller;
 
 import com.example.ojt.exception.CustomException;
+import com.example.ojt.model.dto.request.ChangePasswordRequest;
 import com.example.ojt.model.dto.request.LoginAccountRequest;
 import com.example.ojt.model.dto.request.RegisterAccount;
 import com.example.ojt.model.dto.response.APIResponse;
@@ -10,16 +11,14 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api.myservice.com/v1/auth")
 public class AuthController {
     @Autowired
     private IAccountService accountService;
+
     @PostMapping("/sign-in")
     public ResponseEntity<JWTResponse> doLogin(@Valid @RequestBody LoginAccountRequest loginAccountRequest) throws Exception {
         JWTResponse jwtResponse = accountService.login(loginAccountRequest);
@@ -34,6 +33,53 @@ public class AuthController {
             return new ResponseEntity<>(response, HttpStatus.CREATED);
         } else {
             throw new CustomException("Lack of compulsory registration information or invalid information.", HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+    }
+
+    /**
+     * đăng nhập admin
+     * @param loginAccountRequest
+     * @return
+     * @throws CustomException
+     */
+    @PostMapping("/admin/sign-in")
+    public ResponseEntity<JWTResponse> loginadmin(@RequestBody LoginAccountRequest loginAccountRequest) throws CustomException {
+        JWTResponse jwtResponse = accountService.loginadmin(loginAccountRequest);
+        return ResponseEntity.ok(jwtResponse);
+    }
+
+
+    /**
+     * đăng kí admin
+     * @param registerAccount
+     * @return
+     * @throws CustomException
+     */
+    @PostMapping("/admin/register")
+    public ResponseEntity<?> registerAdmin(@RequestBody @Valid RegisterAccount registerAccount) throws CustomException {
+        boolean check = accountService.registerAdmin(registerAccount);
+        if (check) {
+            APIResponse response = new APIResponse(200, "Register successful");
+            return new ResponseEntity<>(response, HttpStatus.CREATED);
+        } else {
+            throw new CustomException("Lack of compulsory registration information or invalid information.", HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+    }
+
+    /**
+     * thay đổi mật khẩu admin
+     * @param changePasswordRequest
+     * @return
+     * @throws CustomException
+     */
+    @PostMapping("/admin/changepassword")
+    public ResponseEntity<?> changeAdminPassword(@RequestBody @Valid ChangePasswordRequest changePasswordRequest) throws CustomException {
+        boolean check = accountService.changeAdminPassword(changePasswordRequest);
+        if (check) {
+            APIResponse response = new APIResponse(200, "Password changed successfully");
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } else {
+            throw new CustomException("Password change failed", HttpStatus.BAD_REQUEST);
         }
     }
 }
--- src/main/java/com/example/ojt/service/account/AccountService.java
+++ src/main/java/com/example/ojt/service/account/AccountService.java
@@ -1,6 +1,7 @@
 package com.example.ojt.service.account;
 
 import com.example.ojt.exception.CustomException;
+import com.example.ojt.model.dto.request.ChangePasswordRequest;
 import com.example.ojt.model.dto.request.LoginAccountRequest;
 import com.example.ojt.model.dto.request.RegisterAccount;
 import com.example.ojt.model.dto.response.JWTResponse;
@@ -20,32 +21,44 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+
 @Service
 public class AccountService implements IAccountService {
+
     @Autowired
     private AuthenticationManager manager;
+
     @Autowired
     private IAccountRepository accountRepository;
+
     @Autowired
     private PasswordEncoder passwordEncoder;
+
     @Autowired
     private JWTProvider jwtProvider;
+
     @Autowired
     private IRoleRepository roleRepository;
+
     @Override
     public JWTResponse login(LoginAccountRequest loginAccountRequest) throws CustomException {
-        // Xac thuc email and password
-        Authentication authentication = null;
+        // Authenticate email and password
+        Authentication authentication;
         try {
-            authentication = manager.authenticate(new UsernamePasswordAuthenticationToken(loginAccountRequest.getEmail(), loginAccountRequest.getPassword()));
+            authentication = manager.authenticate(new UsernamePasswordAuthenticationToken(
+                    loginAccountRequest.getEmail(), loginAccountRequest.getPassword()));
         } catch (AuthenticationException e) {
             throw new CustomException("Email or password incorrect", HttpStatus.NOT_FOUND);
         }
+
         AccountDetailsCustom detailsCustom = (AccountDetailsCustom) authentication.getPrincipal();
         if (detailsCustom.getStatus() == 2) {
             throw new CustomException("Account has been blocked!", HttpStatus.FORBIDDEN);
         }
+
         String accessToken = jwtProvider.generateAccessToken(detailsCustom);
+
         return JWTResponse.builder()
                 .email(detailsCustom.getEmail())
                 .roleName(detailsCustom.getRoleName())
@@ -62,15 +75,91 @@ public class AccountService implements IAccountService {
         if (!registerAccount.getPassword().equals(registerAccount.getConfirmPassword())) {
             throw new CustomException("Password do not match!", HttpStatus.BAD_REQUEST);
         }
+
         Role role = roleRepository.findByRoleName(RoleName.valueOf(registerAccount.getRoleName()))
                 .orElseThrow(() -> new CustomException("Role not found", HttpStatus.NOT_FOUND));
+
         Account account = Account.builder()
                 .email(registerAccount.getEmail())
                 .password(passwordEncoder.encode(registerAccount.getPassword()))
                 .status(1)
                 .role(role)
                 .build();
+
         accountRepository.save(account);
+        return true;
+    }
+
+    @Override
+    public JWTResponse loginadmin(LoginAccountRequest loginAccountRequest) throws CustomException {
+        // Authenticate email and password
+        Authentication authentication = null; //  lưu trữ thông tin chi tiết của người dùng đã được xác thực. đại diện cho người dùng hiện đang đăng nhập.
+        try {
+            authentication = manager.authenticate(new UsernamePasswordAuthenticationToken(loginAccountRequest.getEmail(), loginAccountRequest.getPassword()));
+            //sử dụng phương thức manager.authenticate() để xác thực người dùng dựa trên email và mật khẩu được cung cấp
+            // tạo ra một đối tượng mới UsernamePasswordAuthenticationToken, đại diện cho thông tin xác thực (email và mật khẩu) của người dùng cần được xác thực.
+        } catch (AuthenticationException e) {
+            throw new CustomException("Email or password incorrect", HttpStatus.NOT_FOUND);
+        }
+
+        AccountDetailsCustom detailsCustom = (AccountDetailsCustom) authentication.getPrincipal();   //  // Principal : đại diện cho người dùng đã đăng nhập.
+        if (detailsCustom.getStatus() == 2) {
+            throw new CustomException("Account has been blocked!", HttpStatus.FORBIDDEN);
+        }
+
+        String accessToken = jwtProvider.generateAccessToken(detailsCustom); // tạo 1 token cho ng dùng đã xác thực.chứa tt cơ bản về ng dùng
+
+        if (!Objects.equals(detailsCustom.getRoleName(), RoleName.ROLE_ADMIN.name())) {
+            throw new CustomException("You are not an Admin!", HttpStatus.FORBIDDEN);
+        }
+
+
+        return JWTResponse.builder()
+                .email(detailsCustom.getEmail())
+                .roleName(detailsCustom.getRoleName())
+                .status(detailsCustom.getStatus())
+                .accessToken(accessToken)
+                .build();
+    }
+
+
+
+    @Override
+    public boolean registerAdmin(RegisterAccount registerAccount) throws CustomException {
+        if (accountRepository.existsByEmail(registerAccount.getEmail())) {
+            throw new CustomException("Email existed!", HttpStatus.CONFLICT);
+        }
+        if (!registerAccount.getPassword().equals(registerAccount.getConfirmPassword())) {
+            throw new CustomException("Password do not match!", HttpStatus.BAD_REQUEST);
+        }
+        Account account = Account.builder()
+                .email(registerAccount.getEmail())
+                .password(passwordEncoder.encode(registerAccount.getPassword()))
+                .status(1)
+                .role(roleRepository.findByRoleName(RoleName.valueOf("ROLE_ADMIN")).orElseThrow(() -> new CustomException("Role not found", HttpStatus.NOT_FOUND)))
+                .build();
+
+        accountRepository.save(account);
+
+        return true;
+    }
+
+    @Override
+    public boolean changeAdminPassword(ChangePasswordRequest changePasswordRequest) throws CustomException {
+        Account account = accountRepository.findByEmail(changePasswordRequest.getEmail())
+                .orElseThrow(() -> new CustomException("Email not found", HttpStatus.NOT_FOUND));
+
+        if (!Objects.equals(account.getRole().getRoleName(), RoleName.ROLE_ADMIN)) {
+            throw new CustomException("Only admin can change the password using this endpoint", HttpStatus.FORBIDDEN);
+        }
+
+        if (!passwordEncoder.matches(changePasswordRequest.getOldPassword(), account.getPassword())) {
+            throw new CustomException("Old password is incorrect", HttpStatus.BAD_REQUEST);
+        }
+
+        account.setPassword(passwordEncoder.encode(changePasswordRequest.getNewPassword()));
+        accountRepository.save(account);
+
         return true;
     }
 }
